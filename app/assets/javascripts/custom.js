6a4f980698e2bf163edcfa9e264e5ef5281af713
Guanghui Li <liguanghui.cs@gmail.com>
2019-03-14 19:51:52 +0000

    added more custom styles and script

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,8 +1,8 @@
 window.onload=function(){
   document.getElementById("livestock_image").addEventListener("input", function(){
     console.log("Hello");
-   var fullPath = this.value;
-   var filename = fullPath.replace(/^.*[\\\/]/, '');
-   document.getElementById("image-label").innerHTML = filename;
+  var fullPath = this.value;
+  var filename = fullPath.replace(/^.*[\\\/]/, '');
+  document.getElementById("image-label").innerHTML = filename;
  });
 }
