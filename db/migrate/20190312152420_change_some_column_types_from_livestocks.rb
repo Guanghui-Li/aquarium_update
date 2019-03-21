4ba05942684176970af47952b9454d844f4128ca
Guanghui Li <liguanghui.cs@gmail.com>
2019-03-21 03:08:36 +0000

    modified migration files for production

--- db/migrate/20190312152420_change_some_column_types_from_livestocks.rb
+++ db/migrate/20190312152420_change_some_column_types_from_livestocks.rb
@@ -1,6 +1,5 @@
 class ChangeSomeColumnTypesFromLivestocks < ActiveRecord::Migration[5.0]
   def change
-    change_column :livestocks, :type, :integer
     change_column :livestocks, :species, :integer
     change_column :livestocks, :tank, :integer
   end
--- db/migrate/20190312154013_change_column_type_to_stock_type_from_livestocks.rb
+++ db/migrate/20190312154013_change_column_type_to_stock_type_from_livestocks.rb
@@ -1,5 +1,6 @@
 class ChangeColumnTypeToStockTypeFromLivestocks < ActiveRecord::Migration[5.0]
   def change
     rename_column :livestocks, :type, :stock_type
+    change_column :livestocks, :stock_type, :integer
   end
 end
